f124a935661af1ac976ae976fc1bd5f51751d3c2
FeiRouZzz <167169441+FeiRouZzz@users.noreply.github.com>
2024-04-16 14:46:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.ch8si64octqy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "limjunxiang-employee"
+customregion = "us-east-1"
+
